55906e896de7869d4d1d9960b8babd9e5d36975d
Isaac Murchie <imurchie@gmail.com>
2013-12-18 11:40:58 -0800

    Add async handling

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,26 @@
-function start() {
+var exec = require("child_process").exec;
+
+function start(response) {
   console.log("Request handler 'start' was called.");
+
+  exec("ls -la", function(error, stdout, stderr) {
+    setContentType(response);
+    response.write(stdout);
+    response.end();
+  });
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
+  setContentType(response);
+  response.write("Hello Upload");
+  response.end();
+}
+
+function setContentType(response) {
+  response.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
 }
 
 exports.start = start;
